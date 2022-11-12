f8484aa61f801a371f2f294404c85b46ec5ebc0f
Martin <martindrus@outlook.degit config --global user.name Martingit config --global user.email martindrus@outlook.de>
2022-11-13 00:10:40 +0100

    -

--- js/script.js
+++ js/script.js
@@ -1,81 +1,89 @@
 
 let playGround = document.querySelector("#playground");
 let playGroundBorderSize = parseInt(getComputedStyle(playGround,null).getPropertyValue('border-width').slice(0, -2));
+let playGroundWidth = parseInt(getComputedStyle(playGround).width.slice(0, -2));
+let playGroundHeight = parseInt(getComputedStyle(playGround).height.slice(0, -2));
 
 
 let theSnake = document.querySelector("#snake");
+let snakeHeadSize = parseInt(getComputedStyle(theSnake).width.slice(0, -2));
 let currentPosX = theSnake.getBoundingClientRect().left - (playGround.getBoundingClientRect().left + playGroundBorderSize);
 let currentPosY = theSnake.getBoundingClientRect().top - (playGround.getBoundingClientRect().top + playGroundBorderSize);
 
+
 //defaultSpeed
-const motionSpeed = 10;
+const motionSpeed = 1;
 //defaultDirection
 let currentDirection = "ArrowRight";
 
-
-
 document.addEventListener("keydown", changeDirection);
-
-
-
 
 function changeDirection(e){
     let directionSelectedByUser = e.key;
-    let newPosX;
-    let newPosY;
 
-
-
-
-    if (currentDirection !== directionSelectedByUser) {
-        switch (directionSelectedByUser) {
-            case "ArrowUp":
-                newPosY = letsGoSnake(currentPosY)
-
-                currentDirection = "ArrowUp";
-                break;
-            case "ArrowDown":
-                newPosY = letsGoSnake(currentPosY)
-
-                currentDirection = "ArrowDown";
-                break;
-            case "ArrowRight":
-                newPosX = letsGoSnake(currentPosX)
-                
-                currentDirection = "ArrowRight";
-                break;
-            case "ArrowLeft":
-                newPosX = letsGoSnake(currentPosX)
-
-                currentDirection = "ArrowLeft";
-                break;  
-            default:
-                break;
-        }
-
-        if (currentDirection === "ArrowRight" || currentDirection === "ArrowLeft") {
-            theSnake.style.left = `${newPosX}px`
-        } else if (currentDirection === "ArrowUp" || currentDirection === "ArrowDown") {
-            theSnake.style.top = `${newPosY}px`
-        }
-
+    switch (directionSelectedByUser) {
+        case "ArrowRight":
+            if (currentDirection !== "ArrowRight" && currentDirection !== "ArrowLeft") {
+                currentDirection = directionSelectedByUser;
+            }
+            break;
+        case "ArrowLeft":
+            if (currentDirection !== "ArrowRight" && currentDirection !== "ArrowLeft") {
+                currentDirection = directionSelectedByUser;
+            }
+            break;
+        case "ArrowUp":
+            if (currentDirection !== "ArrowUp" && currentDirection !== "ArrowDown") {
+                currentDirection = directionSelectedByUser;
+            }
+            break;
+        case "ArrowDown":
+            if (currentDirection !== "ArrowUp" && currentDirection !== "ArrowDown") {
+                currentDirection = directionSelectedByUser;
+            }
+            break;
+        default:
+            break;
     }
-
-
-
 }
 
-
-setInterval((e) => {
-    let test = letsGoSnake()
-    console.log(test);
-    console.log(e);
-}, 2000);
+// setInterval(() => {
+//    letsGoSnake()
+// }, 100);
 
 
+function letsGoSnake() {
 
-function letsGoSnake(currentPosXY) {
-    return currentPosXY + motionSpeed;
+    switch (currentDirection) {
+        case "ArrowUp":
+            if (currentPosY > 0) {
+                currentPosY -= motionSpeed;
+            }
+            break;
+        case "ArrowDown":
+            if (currentPosY < (playGroundHeight - snakeHeadSize)) {
+                currentPosY += motionSpeed;
+            }
+            break;
+        case "ArrowRight":
+            if (currentPosX < (playGroundWidth - snakeHeadSize)) {
+                currentPosX += motionSpeed;
+            }
+            break;
+        case "ArrowLeft":
+            if (currentPosX > 0) {
+                currentPosX -= motionSpeed;
+            }
+            break;  
+        default:
+            break;
+    }
+
+    if (currentDirection === "ArrowRight" || currentDirection === "ArrowLeft") {
+        theSnake.style.left = `${currentPosX}px`
+    } else if (currentDirection === "ArrowUp" || currentDirection === "ArrowDown") {
+        theSnake.style.top = `${currentPosY}px`
+    }
 }
 
 
